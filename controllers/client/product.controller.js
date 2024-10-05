4b670b385c194ae5bec75ba57e57bb128d31eb0e
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2024-10-06 00:03:46 +0700

    Add search products

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,9 +1,21 @@
 const Product = require("../../models/product.model")
+const searchHelper = require("../../helpers/search")
 
 module.exports.index = async (req, res) => {
-    // Call database 
-    const products = await Product.find({})
+    let find = {
+        deleted: false
+    }
 
+    // Search
+    const objectSearch = searchHelper(req.query)
+    console.log(objectSearch)
+    if(objectSearch.keyword){
+        find.title = objectSearch.regex
+    }
+
+    // Call database 
+    const products = await Product.find(find)
+    
     const newProducts = products.map(item => {
         item.newPrice = (item.oldPrice * (100 - item.discountPercent) / 100).toFixed(1)
         return item
@@ -11,6 +23,7 @@ module.exports.index = async (req, res) => {
 
     res.render("client/page/products", {
         pageTitle: "Danh sách sản phẩm",
-        products: newProducts
+        products: newProducts,
+        keyword: objectSearch.keyword
     })
 }
--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,26 @@ let url = new URL(window.location.href)
 
 const zoomImages = document.querySelectorAll("[zoom-image]")
 if(zoomImages.length > 0) {
-    console.log(zoomImages)
     zoomImages.forEach(image => {
         image.addEventListener("click", () => {
             window.open(`${image.src}`)
         })
+    })
+}
+
+//Form Search
+const formSearch = document.querySelector("#form-search")
+if(formSearch){
+
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const keyword = e.target.elements.keyword.value
+        if(keyword){
+            url.searchParams.set("keyword", keyword)
+        }
+        else{
+            url.searchParams.delete("keyword")
+        }
+        window.location.href = url.href
     })
 }
